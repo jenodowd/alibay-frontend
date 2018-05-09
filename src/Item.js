78805272247f71f05725078aeb35efb3e086c6bb
csong111 <caroline.song@mail.mcgill.ca>
2018-05-09 16:52:33 -0400

    item

--- src/Item.js
+++ src/Item.js
@@ -9,7 +9,6 @@ class Item extends Component {
       <div>
           <img src={this.props.image} alt="null" />
           <div>{this.props.name}</div>
-          <div>{this.props.description}</div>
           <div>${this.props.price}</div>
       </div>
       </div>
