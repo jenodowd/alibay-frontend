77377e59e353ce5e6afa3d2d2e687fccb10bdd30
Jen <jenodowd@me.com>
2018-05-09 15:09:32 -0400

    update

--- src/SideNav.js
+++ src/SideNav.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+import Search from './Search.js';
 import './App.css';
 
 class SideNav extends Component {
@@ -10,8 +11,14 @@ class SideNav extends Component {
   render() {
     return (
       <div className="sideNav">
+        <Search />
         <ul className="sideNavUL">
-          <li className="sideNavLI">Sort by:</li>
+          <li className="sideNavTitle">Sort by:</li>
+          <li className="sideNavTitle">Price</li>
+          <li className="sideNavLI">Low to High</li>
+          <li className="sideNavLI">High to Low</li>
+          <li className="sideNavTitle">Recently Added</li>
+          <li className="sideNavTitle">Deals</li>
         </ul>
       </div>
           )
