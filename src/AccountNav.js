8567b38baab2350bf0c515accfc0d1aa660d4cd9
aishad <aishadiallo@gmail.com>
2018-05-10 20:16:58 -0400

    changed nav

--- src/AccountNav.js
+++ src/AccountNav.js
@@ -15,12 +15,11 @@ class AccountNav extends Component {
       <div className = "space"></div>
       <div className="accountNav">
         <ul className="accountNavUL">
-          {this.props.name && <li className="accountNavLI">Welcome {this.props.name} &nbsp; </li>}
-          {this.props.name && <li className="accountNavLI"><Link className="link" to={'/viewaccount'}>View account</Link> &nbsp; </li>}
+          {this.props.name && <li className="accountNavLI"><Link className="link" to={'/viewaccount'}>View account({this.props.name})</Link> &nbsp; </li>}
           {!this.props.name && <li className="accountNavLI"><button className="link" onClick={this.props.renderSignIn}>Login</button> &nbsp; </li>}
           {!this.props.name && <li className="accountNavLI"><button className="link" onClick={this.props.renderSignUp}>Sign Up</button> &nbsp; </li>}
           {this.props.name && <li className="accountNavLI"><button className="postItem" onClick={this.props.renderCreateListing}>POST ITEM</button> &nbsp; </li>}
-    {this.props.name && <li className="accountNavLI"><button><Link className="link" to={'/cart/'+ this.props.userID}>Shopping Cart ({this.props.counter})</Link></button></li>}
+          {this.props.name && <li className="accountNavLI"><button><Link className="link" to={'/cart/'+ this.props.userID}>Shopping Cart ({this.props.counter})</Link></button></li>}
         </ul>
       </div>
       </div>
